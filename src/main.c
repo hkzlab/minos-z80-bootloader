84037073b48be1795941e91a6c086ad09ea3ae50
Fabio Battaglia <hkzlabnet@gmail.com>
2014-06-04 18:18:25 +0200

    Some more cleanup

--- src/main.c
+++ src/main.c
@@ -1,21 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include <common_datatypes.h>
-
 #include "utilities.h"
 
-void sys_init(void);
-
-/** Here lies the code **/
-void sys_init(void) {
-	;
-}
-
 void main(void) {
-	sys_init();
-
 	while(1) { // Endless loop
 		;
 	}
