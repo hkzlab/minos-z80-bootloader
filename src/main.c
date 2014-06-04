c8d279a206f71bfddc06e953d4426167d2d1ca7f
Fabio Battaglia <hkzlabnet@gmail.com>
2014-06-04 18:12:20 +0200

    Add empty codebase

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <common_datatypes.h>
+
+#include "utilities.h"
+
+void sys_init(void);
+
+/** Here lies the code **/
+void sys_init(void) {
+	;
+}
+
+void main(void) {
+	sys_init();
+
+	while(1) { // Endless loop
+		;
+	}
+}
+
