70ea81cc0f3151074f3f749b07afce3b46d7a62d
Fabio Battaglia <hkzlabnet@gmail.com>
2014-06-04 19:20:48 +0200

    Prepare function pointers from ROM

--- src/main.c
+++ src/main.c
@@ -3,7 +3,19 @@
 #include "utilities.h"
 #include "rom/rom_functions.h"
 
+void sys_init(void);
+
+static rom_getchar getchar;
+static rom_putchar putchar;
+
+void sys_init(void) {
+	getchar = (rom_getchar)(*(uint8_t*)(ROM_TABLE_BASE-4));
+	putchar = (rom_putchar)(*(uint8_t*)(ROM_TABLE_BASE-2));
+}
+
 void main(void) {
+	sys_init();
+
 	while(1) { // Endless loop
 		;
 	}
